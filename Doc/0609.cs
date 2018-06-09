35871785a57e3851c65d86e8b30b3bbd894a8803
tjp001 <JiepengTan001@gmial.com>
2018-06-09 22:34:11 +0800

    add some doc

---
+++ Doc/0609.cs
@@ -0,0 +1,28 @@
+参考游戏：三位一体2
+
+v0.0.1
+特性：
+主角 
+移动：
+上下左右 & 跳跃  //TODO&飞行
+防具：盾牌 可反弹
+武器：鞭炮
+
+怪物：
+.被动攻击
+主动
+.近战
+.远程
+
+
+//
+操作和碰撞是2D
+模型特效场景3D
+
+Tools :
+1.ProBuilder
+https://www.youtube.com/watch?v=7k-81UEluyg&t=599s
+2.Cinemachine for 2D
+https://www.youtube.com/watch?v=tDX2ChhntD8&list=PLX2vGYjWbI0RX4rWxhg15UJ9GGf3tFLNQ&index=1
+https://www.youtube.com/watch?v=x6Q5sKXjZOM&list=PLX2vGYjWbI0TQpl4JdfEDNO1xK_I34y8P
+
